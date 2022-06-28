fb3ffce07ad08947832e0821df414e2eb614643f
shubham533 <shubhambanga1265@gmail.com>
2022-06-28 17:41:06 +0530

    changes

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,11 +3,13 @@ import user from './auth/reducer';
 import customer from './requests/reducer';
 import socket from './socket/reducer';
 import notification from './notification/reducer';
+import location from './location/reducer';
 
 const rootreducer = combineReducers({
   user,
   customer,
   socket,
   notification,
+  location,
 });
 export default rootreducer;
